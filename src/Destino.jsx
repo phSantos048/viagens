380d449b62634d12f8d4131ae0d0993db221003b
phSantos048 <128161966+phSantos048@users.noreply.github.com>
2024-08-06 12:05:12 -0300

    Melhorias Visuais

---
+++ src/Destino.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+
+const Destino = ({ nome, descricao }) => {
+  return (
+    <div className="destino">
+      <h2>{nome}</h2>
+      <p>{descricao}</p>
+    </div>
+  );
+};
+
+export default Destino;
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer><p>
+ 2024 Viagem App</p></footer>;
+};
+
+export default Footer;
+
